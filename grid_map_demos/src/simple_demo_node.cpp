55635f95eb36ca93f09af89b773829c3ec55103a
exocrist1 <hassanpourmohammad64@gmail.com>
2023-02-25 00:33:37 +0330

    working gridmap with eudem dataset

--- grid_map_demos/src/simple_demo_node.cpp
+++ grid_map_demos/src/simple_demo_node.cpp
@@ -5,7 +5,7 @@
 #include <memory>
 #include <utility>
 
-#include <jsoncpp/json/json.h>
+#include <nlohmann/json.hpp>
 #include <curl/curl.h>
 #include <string.h>
 
@@ -15,19 +15,14 @@
 // #include <Poco/JSON/Parser.h>
 // #include <Poco/JSON/Object.h>
 
-void shiftedCoordinates(double home_lat, double home_lon, double dx, double dy, double *shiftedLat, double *shiftedLon){
-  double R = 6378137;
+using json = nlohmann::json;
+
+void shiftCoordinates(double home_lat, double home_lon, double dx, double dy, double *shiftedLat, double *shiftedLon){
+  double R = 6378137.0;
   double dlon = dx/R;
   double dlat = dy/R;
   *shiftedLon = home_lon + dlon;
   *shiftedLat = home_lat + dlat;
-}
-
-std::string reqURL(double lat, double lon){
-  
-  std::string request_url = "https://api.opentopodata.org/v1/eudem25m?locations=" + std::to_string(lat) + "," + std::to_string(lon);
-  // std::cout << ">>>>>>>>>>>>>>>>>>>>>>>>requested url: " << request_url << std::endl;
-  return request_url;
 }
 
 static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
@@ -36,31 +31,40 @@ static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *use
     return size * nmemb;
 }
 
-
 double getElevation(double lat, double lon) {
-  auto x = lon + lat;
-  reqURL(lat, lon);
-  
-  CURL *curl;
-  //CURLcode res;
-  std::string readBuffer;
-  curl = curl_easy_init();
-      if(curl) {
-            std::cout<< "curl initiated" << std::endl;
-            curl_easy_setopt(curl, CURLOPT_URL, "https://api.opentopodata.org/v1/eudem25m?locations=32.330000,43.200000");
-            // curl_easy_setopt(curl, CURLOPT_URL, reqURL(lat, lon));
-            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
-            curl_easy_perform(curl);
-            curl_easy_cleanup(curl);
-      }
-  std::cout<< readBuffer << "." << std::endl;
-  return x;
+    // set the API endpoint and the coordinate to retrieve elevation for
+    std::string url = "https://api.opentopodata.org/v1/eudem25m?locations=";
+    //double lat = 48.8584;
+    //double lon = 2.2945;
+    url += std::to_string(lat) + "," + std::to_string(lon);
+
+    // use curl to make the API request and retrieve the response
+    CURL *curl;
+    //CURLcode res;
+    std::string readBuffer;
+
+    curl = curl_easy_init();
+    if(curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+        curl_easy_perform(curl);
+        curl_easy_cleanup(curl);
+    }
+    // parse the JSON response and extract the elevation value
+    json j = json::parse(readBuffer);
+    double elevation = j["results"][0]["elevation"];
+    //std::cout << readBuffer;
+    // print the elevation value
+    //std::cout << "Elevation at (" << lat << ", " << lon << ") is " << elevation << " meters." << std::endl;
+
+    return elevation;
 }
 
 int main(int argc, char ** argv)
 {
   // Initialize node and publisher.
+  double sLat = 0.0, sLon = 0.0, elv=0.0;
   rclcpp::init(argc, argv);
   rclcpp::Node::SharedPtr node = std::make_shared<rclcpp::Node>("grid_map_simple_demo");
   auto publisher = node->create_publisher<grid_map_msgs::msg::GridMap>(
@@ -69,14 +73,14 @@ int main(int argc, char ** argv)
   // Create grid map.
   grid_map::GridMap map({"elevation"});
   map.setFrameId("map");
-  map.setGeometry(grid_map::Length(3, 3), 0.03);
+  map.setGeometry(grid_map::Length(1.0, 1.0), 0.1);
   RCLCPP_INFO(
     node->get_logger(),
     "Created map with size %f x %f m (%i x %i cells).",
     map.getLength().x(), map.getLength().y(),
     map.getSize()(0), map.getSize()(1));
 
-  getElevation(32.33, 43.2);
+  getElevation(61.806815, 6.625353);
   // Work with grid map in a loop.
   rclcpp::Rate rate(30.0);
   rclcpp::Clock clock;
@@ -86,12 +90,15 @@ int main(int argc, char ** argv)
     for (grid_map::GridMapIterator it(map); !it.isPastEnd(); ++it) {
       grid_map::Position position;
       map.getPosition(*it, position);
+      shiftCoordinates(61.806815, 6.625353, (double)position.x()*100000.0, (double)position.y()*100000.0, &sLat, &sLon);
+      elv = getElevation(sLat,sLon);
+      std::cout<<elv<<"\n";
       map.at(
         "elevation",
-        *it) = -0.04 + 0.2 * std::sin(3.0 * time.seconds() + 5.0 * position.y()) * position.x();
+        *it) = elv/1500.0;/*-0.04 + 0.2 * std::sin(3.0 * time.seconds() + 5.0 * position.y()) * position.x();*/
     }
-    getElevation(32.33, 43.2);
-
+    //getElevation(61.806815, 6.625353);
+    
     // Publish grid map.
     map.setTimestamp(time.nanoseconds());
     std::unique_ptr<grid_map_msgs::msg::GridMap> message;
